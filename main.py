8b041364a944351e3aacd47d8c08e2f4d0270e39
Ismael Monje <salime45@gmail.com>
2017-02-06 21:50:31 +0100

    Agregado otro pcap de ejemplo; Controlado si existe layer IP

--- main.py
+++ main.py
@@ -15,13 +15,16 @@ def savePcap(path):
         p = packets[i]
         print("MAC: " + p.src)
         print("TIME: " + getDate(p.time))
-        print("IP: " +  packets[i][IP].dst)
+        
+        if IP in p:            
+            print("IP: " +  packets[i][IP].dst)
 
         if scapyh.HTTPRequest in p:            
             print("User-Agent: " + str(scapyh._get_field_value(p[scapyh.HTTPRequest], "User-Agent")))
             print("Host: " + str(scapyh._get_field_value(p[scapyh.HTTPRequest], "Host")))
 
         print("=====================================================================================\n")
+        
         '''
         perfil = Perfil()
         perfil.mac = p.src
@@ -31,7 +34,7 @@ def getDate(time):
     return datetime.datetime.fromtimestamp(time).strftime('%d-%m-%Y %H:%M:%S')
         
         
-savePcap("cap/example.pcapng")
+savePcap("cap/example2.pcapng")
 
 
 
